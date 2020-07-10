8afd9899412d01243bd54d8872fe1de9daa2840a
David Mann <David Mann@192.168.0.12>
2020-07-10 22:53:11 +0900

    Created the public folder

--- GeneralUnion/public/js/crud/jsdoc.bat
+++
@@ -1,5 +0,0 @@
-set jsdoc_path="C:\Users\David Mann\AppData\Roaming\npm\jsdoc" 
-set project_path=C:\GUDB\gudb0601\GeneralUnion\public\js\ready_for_docs\
-set config_path=C:\GUDB\gudb0601-jsdoc\config\conf.json
-set out_path=C:\GUDB\gudb0601-jsdoc\out\jsdoc
-%jsdoc_path% %project_path% -r -c %config_path% -d %out_path%
